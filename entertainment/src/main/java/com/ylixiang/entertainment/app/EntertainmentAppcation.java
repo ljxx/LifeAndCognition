d8ddde649234a17488fe609df857b31d9f9aab25
yanglixiang <yanglixiang@zhaoonline.com>
2018-12-04 17:32:13 +0800

    添加生活、集市、工具、娱乐、用户模块

---
+++ entertainment/src/main/java/com/ylixiang/entertainment/app/EntertainmentAppcation.java
@@ -0,0 +1,23 @@
+package com.ylixiang.entertainment.app;
+
+import android.app.Application;
+
+/**
+ * ========================================
+ * <p/>
+ * 版 权：蓝吉星讯 版权所有 （C） 2017
+ * <p/>
+ * 作 者：yanglixiang
+ * <p/>
+ * 版 本：1.0
+ * <p/>
+ * 创建日期：2018/12/4  下午5:16
+ * <p/>
+ * 描 述：
+ * <p/>
+ * 修订历史：
+ * <p/>
+ * ========================================
+ */
+public class EntertainmentAppcation extends Application {
+}
---
+++ market/src/main/java/com/ylixiang/market/MarketMainActivity.java
@@ -0,0 +1,13 @@
+package com.ylixiang.market;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarketMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_main);
+    }
+}
